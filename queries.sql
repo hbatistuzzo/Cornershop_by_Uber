e7170745a122ed05816898d764fece22cbbc0480
HBatistuzzo <hbatistuzzo@gmail.com>
2022-11-21 02:35:24 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -29,10 +29,19 @@ JOIN storebranch on storebranch.store_branch_id = orders.store_branch_id
 GROUP BY orders.order_id; -- OH OH 
 
 -- 3. Calculate the average found rate(*) of the orders grouped by the product format and day of the week.
+-- (*) Found Rate = ratio between the quantity product found vs the number of products ordered by the customer.
+
+SELECT * FROM order_product;
+
+SELECT order_id, COUNT(DISTINCT(product_id)), quantity_found
+FROM order_product
+GROUP BY order_id;
 
 -- 4. Calculate the average error and mean squared error of our estimation model for each hour of the day.
 
 -- 5. Calculate the number of orders in which the picker_id and driver_id are different.
+SELECT COUNT(*) from orders
+WHERE picker_id != driver_id;
 
 
 
